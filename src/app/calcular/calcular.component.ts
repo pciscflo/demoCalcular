b012726d854130644f8bc06d2188cbc9dd7ebeb0
professor <pciscflo@upc.edu.pe>
2024-05-14 01:04:31 -0500

    Producto Entidad

--- src/app/calcular/calcular.component.ts
+++ src/app/calcular/calcular.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Producto } from '../model/producto';
 
 @Component({
   selector: 'app-calcular',
@@ -8,19 +9,20 @@ import { Component, OnInit } from '@angular/core';
 export class CalcularComponent implements OnInit {
   pago: number = 0
   consumo: any = 0
+  producto: Producto = new Producto;
   ngOnInit(): void {
      this.consumo = "";
   }
 
   calcularPago(): void {
     if (this.consumo <= 4) {
-      this.pago = 50
+      this.producto.pago = 50
     }
     if (this.consumo > 4 && this.consumo <= 8) {
-      this.pago = 85
+      this.producto.pago = 85
     }
     if (this.consumo > 8) {
-      this.pago = 85 + 4.5 * (this.consumo - 8)
+      this.producto.pago = 85 + 4.5 * (this.consumo - 8)
     }
   }
 
